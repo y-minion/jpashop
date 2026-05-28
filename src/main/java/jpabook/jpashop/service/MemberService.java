2a40ddb2553f5aa197f6d59292535872b3e7bf25
y-minion <dudals2963@tukorea.ac.kr>
2026-05-29 00:56:12 +0900

    인프런 JPA+SPRING강의 코드베이스 깃허브 업로드 init

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,16 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
 
     /**
      * 회원가입
      */
+    @Transactional
     public Long join(Member member) {
+
         validataDuplicateMember(member);
 
         memberRepository.save(member);
