fd57daab3128869064d3d04619587b03819b8629
y-minion <dudals2963@tukorea.ac.kr>
2026-06-16 00:45:21 +0900

    회원목록 조회 할 차례

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -2,10 +2,12 @@ package jpabook.jpashop.controller;
 
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,11 +23,22 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    //POST를 통해 폼을 받으면 폼에서 데이터를 꺼내 객체를 만들어준뒤 멤버 서비스에 저장하고 홈으로 리다이렉트를 시켜준다.
     @PostMapping("members/new")
-    public String create(@Valid MemberForm form) { //@Valide 어노테이션만 사용하면 뷰의 폼에서 전달한 폼의 값들에 대해 폼 클래스에서 적용한 어노테이션에 대한 유효성 검사를 해주나?
+    public String create(@Valid MemberForm form, BindingResult result) { //@Valide 어노테이션만 사용하면 뷰의 폼에서 전달한 폼의 값들에 대해 폼 클래스에서 적용한 어노테이션에 대한 유효성 검사를 해주나?
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
 
         //뷰에서 post로 보낸 폼의 데이터를 꺼내 객체로 만든다.
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setUsername(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+        return "redirect:/";
 
     }
 }
