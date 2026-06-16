f434bb1ad8e07e45f1485b343aae15400bc3ef05
y-minion <dudals2963@tukorea.ac.kr>
2026-06-17 00:35:58 +0900

    상품 수정 할 차례

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        // 컨트롤러 메서드 파라미터에 객체를 적어두면, 요청 파라미터들을 이 객체에 자동으로 바인딩해준다.
+        // form HTML에서 받은 정보에 대한 엔티티? 객체가 POST 매핑 메서드의 파라미터로 전달된다.
+
+        //받아온 정보를 Book 엔티티에 넣어서 DB에 저장한다.
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,20 +11,22 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("members/new")
+    @GetMapping("/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm()); // 왜 폼을 모델에 넣어저 전달할까?
         return "members/createMemberForm";
     }
 
     //POST를 통해 폼을 받으면 폼에서 데이터를 꺼내 객체를 만들어준뒤 멤버 서비스에 저장하고 홈으로 리다이렉트를 시켜준다.
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String create(@Valid MemberForm form, BindingResult result) { //@Valide 어노테이션만 사용하면 뷰의 폼에서 전달한 폼의 값들에 대해 폼 클래스에서 적용한 어노테이션에 대한 유효성 검사를 해주나?
 
         if (result.hasErrors()) {
@@ -40,5 +42,13 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
 
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
